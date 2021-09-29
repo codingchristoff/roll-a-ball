f505c1d462a2c4ad12f11f4e88f23d6791bad85e
Chris Lowe <codingchristoff@gmail.com>
2021-09-29 19:08:33 +0100

    add ui elements and completed build.
    -version 1 complete

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
     private float movementX, movementY;
+    private int count;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void FixedUpdate()
@@ -27,14 +34,26 @@ public class PlayerController : MonoBehaviour
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
-    }   
+    }
+
+    void SetCountText()
+    {
+        countText.text = $"Count: {count}";
+
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+                }
+    }
 
     //DETECT AND TEST COLLISIONS ON SPHERE COLLIDER
     void OnTriggerEnter(Collider other)
     {
-      if(other.gameObject.CompareTag("pickUp"))
+        if (other.gameObject.CompareTag("pickUp"))
         {
             other.gameObject.SetActive(false);
-        }            
+            count++;
+            SetCountText();
+        }
     }
 }
